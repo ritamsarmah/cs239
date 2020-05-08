ccba06cbc12f1a160b584481c72d3b31a2f0771e
ritamsarmah <sarmah.ritam@gmail.com>
2020-05-07 22:51:17 -0700

    Update testing output

--- main.py
+++ main.py
@@ -5,38 +5,41 @@ import time
 
 
 def test_algorithm(tests, algorithm, verbose=True):
-    start = time.time()
-
     if verbose:
-        print(f"\nRunning tests for {algorithm.__name__}")
-    for (test_num, (test_input, test_output)) in enumerate(tests):
-        if verbose:
-            print(
-                f"----------------------------------------------------------------------\nTest {test_num + 1}: {test_input}")
+        print(f"\nRunning tests for {algorithm.__name__}\n" + '-' * 50)
+        print("    n\ttime (s)\toutput\n" + '-' * 50)
+
+    passed = 0
+    total_time = 0
+    for (test_input, test_output) in tests:
+        start = time.time()
 
         output = algorithm(*test_input).run()
+        
+        end = time.time()
+        elapsed = end - start
+        total_time += elapsed
 
         if verbose:
-            print(f"Result: {output}")
+            if output == test_output:
+                print(f"(✓) {test_input[0]}\t{elapsed:.4f}\t\t{output}")
+                passed += 1
+            else:
+                print(
+                    f"(×) {test_input[0]}\t{elapsed:.4f}\t\t{output} (Expected: {test_output})\t")
 
-        assert output == test_output, f"Failed Test {test_num + 1}. Expected {test_output}, but got {output}"
-
-    end = time.time()
-    elapsed = end - start
-
-    print(
-        f"----------------------------------------------------------------------\nRan {len(tests)} tests in {elapsed}s\n\nOK")
+    print('-' * 50 + f"\nPassed {passed}/{len(tests)} tests in {total_time:.4f}s")
 
 
 if __name__ == "__main__":
     tests = [
-        ((1, [0, 0]), 1),
-        ((1, [0, 1]), 0),
-        ((1, [1, 0]), 0),
-        ((1, [1, 1]), 1),
-        ((2, [1, 1, 1, 1]), 1),
-        ((2, [1, 0, 0, 1]), 0),
-        ((3, [1] * (2**3)), 1),
+        ((1, lambda x: [0, 1][x]), 0),
+        ((1, lambda x: [1, 0][x]), 1),
+        ((1, lambda x: 0 ), 1),
+        ((1, lambda x: 1 ), 1),
+        ((2, lambda x: 1), 1),
+        ((2, lambda x: [1, 0, 0, 1][x]), 0),
+        ((3, lambda x: 1), 1),
     ]
 
     test_algorithm(tests, deutsch_jozsa.DeutschJozsa)
